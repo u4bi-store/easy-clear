744472fd01c56a29d8ac858fc86d6a5fd06fddb0
u4bi <u4bi@u4bi.com>
2017-01-25 20:32:57 +0000

    Singleton 패턴 처리 - 백그라운드에서 키입력시 데이터 삭제가 중복되지 않도록 방지함

--- background/back-paper.js
+++ background/back-paper.js
@@ -1,3 +1,17 @@
 var cache_paper = (function (){
     var instance;
+    
+    function init(){
+      return{
+        
+      };
+    }
+    return {
+      getInstance: function(){
+        if(!instance)instance = init();
+        return instance;
+      }
+    }
 })();
+
+var paper = cache_paper.getInstance();
