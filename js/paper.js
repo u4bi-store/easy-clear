2a878532859f9e0a51cbb24ebfdc0ef402a213c8
u4bi <u4bi@u4bi.com>
2017-01-26 01:21:37 +0000

    팝업뷰 설정 리스트 체크박스 렌더링

--- js/paper.js
+++ js/paper.js
@@ -1,3 +1,5 @@
+var dataArray = [];
+
 var data =[
     {name: "응용 프로그램 캐시" ,key: "appcache",    value: false},
     {name: "캐시된 이미지,파일" ,key: "cache",       value: false},
@@ -12,3 +14,29 @@ var data =[
     {name: "비밀번호"         ,key: "passwords",   value: false},
     {name: "webSQL"         ,key: "webSQL",      value: false}
 ];
+
+document.addEventListener('DOMContentLoaded', function () {
+    var box, li, label, removed, checked;
+    
+    box = document.getElementById('box');
+	box.innerHTML='인터넷 사용기록 삭제';
+    
+    for(var i=0; i<data.length; i++){
+        li = document.createElement('li');
+        ul.appendChild(li);
+        
+        label = document.createElement('label');
+        label.innerHTML=data[i].name;
+        li.appendChild(label);
+
+        dataArray[i] = document.createElement("input");
+        dataArray[i].setAttribute("type", "checkbox");
+        dataArray[i].setAttribute("value", data[i].key);
+        dataArray[i].checked = data[i].value;
+        
+        label.appendChild(dataArray[i]);
+    }
+    
+    removed=document.getElementById('removed');
+    checked=document.getElementById('checked');
+});
