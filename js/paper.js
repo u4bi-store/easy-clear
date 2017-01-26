4a21bb58e1eb3fcf546f794900be0accea2992e4
u4bi <u4bi@u4bi.com>
2017-01-26 01:11:07 +0000

    팝업뷰에 출력되는 데이터 객체 모델링 - @ name : 라벨에 보여질 텍스트 - @ key : 로컬스토리지에 저장되고 후에 찾을 키값 - @ value : 키에 저장되는 벨류값 데이터가 삭제될지 안될지를 정함 true삭제/false삭제안함

--- js/paper.js
+++ js/paper.js
@@ -1,1 +1,14 @@
-console.log('paper.js test');
+var data =[
+    {name: "응용 프로그램 캐시" ,key: "appcache",    value: false},
+    {name: "캐시된 이미지,파일" ,key: "cache",       value: false},
+    {name: "쿠키"            ,key: "cookies",     value: false},
+    {name: "다운로드 기록"     ,key: "downloads",   value: false},
+    {name: "파일 시스템즈"     ,key: "fileSystems", value: false},
+    {name: "폼 데이터"        ,key: "formData",    value: false},
+    {name: "히스토리 기록"     ,key: "history",     value: false},
+    {name: "indexedDB"      ,key: "indexedDB",   value: false},
+    {name: "로컬스토리지"      ,key: "localStorage",value: false},
+    {name: "플러그인 데이터"   ,key: "pluginData",  value: false},
+    {name: "비밀번호"         ,key: "passwords",   value: false},
+    {name: "webSQL"         ,key: "webSQL",      value: false}
+];
