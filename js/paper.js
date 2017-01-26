921166beb1189bb4d9524179eb2ca1968a703b32
u4bi <u4bi@u4bi.com>
2017-01-26 01:55:04 +0000

    PaperController 생성자 패턴 처리 프로토타입 정의 - PaperController - 체크박스칸의 체크됨을 컨트롤하고 그 값을 로컬 스토리지에 저장하는 객체 - @param data 뷰단에 출력될 될 데이터 name, key value가 있다. - @param fixCheck 체크박스의 체크됨을 컨트롤하는 함수 (flag) - @param save 키와 벨류를 인자로 넘겨받아 로컬스토리지에 저장해준다.

--- js/paper.js
+++ js/paper.js
@@ -15,7 +15,22 @@ var data =[
     {name: "webSQL"         ,key: "webSQL",      value: false}
 ];
 
+var PaperControl = function(){};
+
+PaperControl.prototype = {
+    fixCheck : function(bool){
+        console.log('fixCheck : '+bool);
+    },
+    save : function(key, value){
+        console.log('save : '+key+' '+value);
+    }
+};
+
+
 document.addEventListener('DOMContentLoaded', function () {
+    
+    var ctrl = new PaperControl();
+    
     var box, li, label, removed, checked;
     
     box = document.getElementById('box');
@@ -33,10 +48,18 @@ document.addEventListener('DOMContentLoaded', function () {
         dataArray[i].setAttribute("type", "checkbox");
         dataArray[i].setAttribute("value", data[i].key);
         dataArray[i].checked = data[i].value;
+
+        dataArray[i].onclick=function(e){
+            ctrl.save(e.target.attributes[1].nodeValue, e.target.checked);};
         
         label.appendChild(dataArray[i]);
     }
     
     removed=document.getElementById('removed');
     checked=document.getElementById('checked');
+    removed.onclick=function(){
+        ctrl.fixCheck(false);};
+
+    checked.onclick=function(){
+        ctrl.fixCheck(true);};
 });
