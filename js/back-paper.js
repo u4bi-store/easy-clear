9e4f0b5385bccba5fafb1a3e2abd7f5b0e25bf91
u4bi <u4bi@u4bi.com>
2017-02-22 01:04:24 +0900

    기록삭제 단축키 이슈 - 백그라운드 js내 리무브 속성 구문 리펙토링

--- js/back-paper.js
+++ js/back-paper.js
@@ -6,34 +6,29 @@ var cache_paper = (function (){
       return{
         stat : false,
         on: function(){
-            console.log('on test');
             instance.remove();
             instance.flag();
         },
         remove : function(){
-            console.log('remove test');
-          
-            var attribue =[
-              {"since": 0},
-              {
-                "appcache"    :JSON.parse(data['appcache']),
-                "cache"       :JSON.parse(data['cache']),
-                "cookies"     :JSON.parse(data['cookies']),
-                "downloads"   :JSON.parse(data['downloads']),
-                "fileSystems" :JSON.parse(data['fileSystems']),
-                "formData"    :JSON.parse(data['formData']),
-                "history"     :JSON.parse(data['history']),
-                "indexedDB"   :JSON.parse(data['indexedDB']),
-                "localStorage":JSON.parse(data['localStorage']),
-                "pluginData"  :JSON.parse(data['pluginData']),
-                "passwords"   :JSON.parse(data['passwords']),
-                "webSQL"      :JSON.parse(data['webSQL'])
-              }
-            ];
-            chrome.browsingData.remove(attribue, instance.flag);
+            chrome.browsingData.remove(
+                {"since": 0},
+                {
+                    "appcache"    :JSON.parse(data['appcache']),
+                    "cache"       :JSON.parse(data['cache']),
+                    "cookies"     :JSON.parse(data['cookies']),
+                    "downloads"   :JSON.parse(data['downloads']),
+                    "fileSystems" :JSON.parse(data['fileSystems']),
+                    "formData"    :JSON.parse(data['formData']),
+                    "history"     :JSON.parse(data['history']),
+                    "indexedDB"   :JSON.parse(data['indexedDB']),
+                    "localStorage":JSON.parse(data['localStorage']),
+                    "pluginData"  :JSON.parse(data['pluginData']),
+                    "passwords"   :JSON.parse(data['passwords']),
+                    "webSQL"      :JSON.parse(data['webSQL'])
+                },
+            instance.flag);
         },
         flag : function(){
-            console.log('flag test');
             instance.stat = !instance.stat;
           
             var attribue ={ path: 'images/icon-cache_'+instance.stat+'.png'};
